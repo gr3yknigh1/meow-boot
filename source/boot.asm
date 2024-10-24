2a6f3f286e87f67f3b601f0b789c085edaecf176
Ilya Akkuzin <gr3yknigh1@gmail.com>
2024-10-24 13:04:01 +0300

    Add log about kernel entry

--- source/boot.asm
+++ source/boot.asm
@@ -5,10 +5,12 @@
 %define INT_VIDEO    10h
 %define INT_DISK     13h   ; @see https://en.wikipedia.org/wiki/INT_13H
 
-; INT_VIDEO (`ah` register)
+; INT_VIDEO 
+; AH - register
 %define INT_VIDEO_TTY_WRITE_CHAR 0eh
+%define INT_VIDEO_SET_MODE       00h
 
-; INT_DISK (`ah` register)
+; AH - register
 %define INT_DISK_READ_SECTORS    02h
 
 ; Other macros
@@ -28,6 +30,18 @@ start:
     call bios_halt
 
 ;
+; void bios_tty_clear(void);
+;
+; @breaf Clears the TTY screen.
+;
+bios_tty_clear:
+    push ax
+    mov ah, INT_VIDEO_SET_MODE
+    mov al, 0x03                        ; @research
+    int INT_VIDEO
+    pop ax
+    ret
+;
 ; [[noreturn]] void bios_halt(void);
 ;
 ; @breaf Halts core and enters infinite loop
@@ -38,25 +52,25 @@ bios_halt:
 	jmp	.bios_halt_loop
 
 ;
-; void puts(char *s);
+; void bios_puts(char *s);
 ;
 ; @breaf Prints characters to the TTY.
 ; @param ds:si Pointer to string to put in TTY
 ;
-puts:
+bios_puts:
 	push si
 	push ax
-    jmp .puts_loop
-.puts_loop:
+    jmp .bios_puts_loop
+.bios_puts_loop:
 	lodsb					; Loads next character in al
 	or al, al				; Is next character is \0
-	jz .puts_done
+	jz .bios_puts_done
 
 	mov ah, INT_VIDEO_TTY_WRITE_CHAR
 	int INT_VIDEO
 
-	jmp .puts_loop
-.puts_done:
+	jmp .bios_puts_loop
+.bios_puts_done:
 	pop ax
 	pop si
 	ret
@@ -77,11 +91,8 @@ bootloader_entry:
 	mov ss, ax
 	mov sp, BOOT_SECTOR_ADDR	; Stack will grows downwards
 
-%if 0
-	; Print hello world
-	mov si, hello_world_message 
-	call puts
-%endif
+    ; Hello there!
+    mov si, message_hello_world
 
     ; Loading kernel 
     mov ah, INT_DISK_READ_SECTORS    ; Subroutine code
@@ -92,30 +103,44 @@ bootloader_entry:
     mov dl, 0x80                     ; Drive number
     mov bx, KERNEL_SECTOR_ADDR
     int INT_DISK
-    jc .kernel_load_error
-    jmp .kernel_load_ok
-.kernel_load_error:
-	mov si, message_kernel_load_error
-	call puts
+    jc .bootloader_entry_kernel_load_error
+    jmp .bootloader_entry_kernel_load_ok
+.bootloader_entry_kernel_load_error:
+	mov si, message_boot_kernel_load_error
+	call bios_puts
     ret
-.kernel_load_ok:
-    mov si, message_kernel_load_ok
-    call puts
+.bootloader_entry_kernel_load_ok:
+    mov si, message_boot_kernel_load_ok
+    call bios_puts
     jmp KERNEL_SECTOR_ADDR
     ret
 
-message_hello_world:	
-    db 'Hello world', ENDLINE, 0
-message_kernel_load_ok:
-    db 'Kernel is loaded!', ENDLINE, 0
-message_kernel_load_error:
-    db 'Failed to load kernel', ENDLINE, 0
+message_boot_hello_world:	
+    db "BOOT: Hello there!", ENDLINE, 0
+message_boot_kernel_load_ok:
+    db "BOOT: Loaded the kernel!", ENDLINE, 0
+message_boot_kernel_load_error:
+    db "BOOT: Failed to load the kernel", ENDLINE, 0
 
 times	510-($-$$)	db	0
 dw		BOOT_SECTOR_MAGIC
 
 jmp kernel_boot
 
+;
+; [[noreturn]] void kernel_boot(void);
+;
+; @breaf Kernel boot startup code.
+;
 kernel_boot:
+    call bios_tty_clear
+
+    mov si, message_kernel_welcome
+    call bios_puts
+
+
+
     call bios_halt
 
+message_kernel_welcome:
+    db "KERNEL: Hello sailor!", ENDLINE, 0
